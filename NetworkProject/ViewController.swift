2b8d0e8c02c73e3339397553ddbe3f59310855b5
Acasiax <117105267+Acasiax@users.noreply.github.com>
2024-06-05 17:23:40 +0900

    Initial Commit

---
+++ NetworkProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetworkProject
+//
+//  Created by 이윤지 on 6/5/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
