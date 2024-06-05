69d15f4eafa9e483a48f368acf8d39ea9791bfa8
Acasiax <117105267+Acasiax@users.noreply.github.com>
2024-06-05 21:47:14 +0900

    viewDidLoad에서 UI 요소 초기화 및 계층 설정

--- NetworkProject/넷플릭스/View/SignUpViewController.swift
+++ NetworkProject/넷플릭스/View/SignUpViewController.swift
@@ -10,12 +10,12 @@ import SnapKit
 
 class SignupViewController: UIViewController {
     let titleLabel = UILabel.createSignupTitleLabel()
-       let emailTextField = UITextField.createSignupTextField(placeholder: "이메일 주소 또는 전화번호")
-       let passwordTextField = UITextField.createSignupTextField(placeholder: "비밀번호", isSecure: true)
-       let nicknameTextField = UITextField.createSignupTextField(placeholder: "닉네임")
-       let locationTextField = UITextField.createSignupTextField(placeholder: "위치")
-       let referralCodeTextField = UITextField.createSignupTextField(placeholder: "추천 코드 입력")
-       
+    let emailTextField = UITextField.createSignupTextField(placeholder: "이메일 주소 또는 전화번호")
+    let passwordTextField = UITextField.createSignupTextField(placeholder: "비밀번호", isSecure: true)
+    let nicknameTextField = UITextField.createSignupTextField(placeholder: "닉네임")
+    let referralCodeTextField = UITextField.createSignupTextField(placeholder: "추천 코드 입력")
+    let locationTextField = UITextField.createSignupTextField(placeholder: "위치")
+    
     let signUpButton = UIButton.createSignupButton(title: "회원가입")
     let moreInfoButton = UIButton.createMoreInfoButton(title: "추가 정보 입력")
     
@@ -28,9 +28,10 @@ class SignupViewController: UIViewController {
     }
     
     func setupViewHierarchy() {
-      
+    emailTextField: emailTextField,
+    passwordTextField: passwordTextField,
     }
     
-  
+    
 }
 
