9444c237e00e877389b0be617c125a4697fb6814
Acasiax <117105267+Acasiax@users.noreply.github.com>
2024-06-05 18:41:23 +0900

    SnapKit을 활용한 오토레이아웃 설정 및 확장 메서드 구현

---
+++ NetworkProject/넷플릭스/Extension/AutoLayout+EXT.swift
@@ -0,0 +1,58 @@
+//
+//  AutoLayout+EXT.swift
+//  NetworkProject
+//
+//  Created by 이윤지 on 6/5/24.
+//
+
+import UIKit
+import SnapKit
+
+extension UIViewController {
+    func setupConstraints(headerImageView: UIImageView, playButton: UIButton, favoriteButton: UIButton, trendingLabel: UILabel, firstTrendingImageView: UIImageView, secondTrendingImageView: UIImageView, thirdTrendingImageView: UIImageView) {
+        headerImageView.snp.makeConstraints {
+            $0.horizontalEdges.equalTo(view.safeAreaLayoutGuide)
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.height.equalTo(view.snp.height).multipliedBy(0.5)
+        }
+        
+        playButton.snp.makeConstraints {
+            $0.leading.equalTo(headerImageView.snp.leading).offset(16)
+            $0.bottom.equalTo(headerImageView.snp.bottom).offset(-16)
+            $0.height.equalTo(40)
+        }
+        
+        favoriteButton.snp.makeConstraints {
+            $0.trailing.equalTo(headerImageView.snp.trailing).offset(-16)
+            $0.bottom.equalTo(headerImageView.snp.bottom).offset(-16)
+            $0.height.equalTo(40)
+        }
+        
+        trendingLabel.snp.makeConstraints {
+            $0.top.equalTo(headerImageView.snp.bottom).offset(16)
+            $0.leading.equalTo(view.safeAreaLayoutGuide).offset(16)
+        }
+        
+        firstTrendingImageView.snp.makeConstraints {
+            $0.top.equalTo(trendingLabel.snp.bottom).offset(8)
+            $0.leading.equalTo(view.safeAreaLayoutGuide).offset(16)
+            $0.width.equalTo(view.snp.width).dividedBy(3).offset(-16)
+            $0.height.equalTo(firstTrendingImageView.snp.width).multipliedBy(1.5)
+        }
+        
+        secondTrendingImageView.snp.makeConstraints {
+            $0.top.equalTo(firstTrendingImageView)
+            $0.leading.equalTo(firstTrendingImageView.snp.trailing).offset(8)
+            $0.width.equalTo(firstTrendingImageView)
+            $0.height.equalTo(firstTrendingImageView)
+        }
+        
+        thirdTrendingImageView.snp.makeConstraints {
+            $0.top.equalTo(firstTrendingImageView)
+            $0.leading.equalTo(secondTrendingImageView.snp.trailing).offset(8)
+            $0.width.equalTo(firstTrendingImageView)
+            $0.height.equalTo(firstTrendingImageView)
+            $0.trailing.equalTo(view.safeAreaLayoutGuide).offset(-16)
+        }
+    }
+}
--- NetworkProject/넷플릭스/View/NetfliexHomeViewController.swift
+++ NetworkProject/넷플릭스/View/NetfliexHomeViewController.swift
@@ -10,28 +10,34 @@ import SnapKit
 
 class NetfliexHomeViewController: UIViewController {
     
-    let headerImageView = UIImageView.createImageView(named: "명량")
-        let playButton = UIButton.createPlayButton()
-        let favoriteButton = UIButton.createFavoriteButton()
-        let trendingLabel = UILabel.createLabel(withText: "지금 뜨는 콘텐츠")
-        let firstTrendingImageView = UIImageView.createImageView(named: "응답하라 1988")
-        let secondTrendingImageView = UIImageView.createImageView(named: "태어난 김에 세계일주")
-        let thirdTrendingImageView = UIImageView.createImageView(named: "코미디 로얄")
+    let headerImageView = UIImageView.createImageView(named: "노량")
+    let playButton = UIButton.createPlayButton()
+    let favoriteButton = UIButton.createFavoriteButton()
+    let trendingLabel = UILabel.createLabel(withText: "지금 뜨는 콘텐츠")
+    let firstTrendingImageView = UIImageView.createImageView(named: "범죄도시3")
+    let secondTrendingImageView = UIImageView.createImageView(named: "아바타물의길")
+    let thirdTrendingImageView = UIImageView.createImageView(named: "육사오")
     override func viewDidLoad() {
         super.viewDidLoad()
         setupViewHierarchy()
-       
+        setupConstraints()
+        configureUI(title: "홈")
     }
-  
+    
     private func setupViewHierarchy() {
-          view.addSubview(headerImageView)
-          view.addSubview(playButton)
-          view.addSubview(favoriteButton)
-          view.addSubview(trendingLabel)
-          view.addSubview(firstTrendingImageView)
-          view.addSubview(secondTrendingImageView)
-          view.addSubview(thirdTrendingImageView)
-      }
-      
-
+        view.addSubview(headerImageView)
+        view.addSubview(playButton)
+        view.addSubview(favoriteButton)
+        view.addSubview(trendingLabel)
+        view.addSubview(firstTrendingImageView)
+        view.addSubview(secondTrendingImageView)
+        view.addSubview(thirdTrendingImageView)
+    }
+    
+    private func setupConstraints() {
+   
+    }
+    
+    
 }
+
