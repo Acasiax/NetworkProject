0dfbdc37f7a7e7189e47913142b3c44fd973f8e4
Acasiax <117105267+Acasiax@users.noreply.github.com>
2024-06-05 22:50:08 +0900

    로또 - 디코딩 구조체 먼저 생성

--- NetworkProject/로또/RotaryHomeViewController.swift
+++ NetworkProject/로또/RotaryHomeViewController.swift
@@ -12,9 +12,17 @@ import SnapKit
 class RotaryHomeViewController: UIViewController {
 
     struct Lotto: Decodable {
-        let drwNoDate: String
-        let totSellamnt: Int
-    }
+         let drwNoDate: String
+         let drwtNo1: Int
+         let drwtNo2: Int
+         let drwtNo3: Int
+         let drwtNo4: Int
+         let drwtNo5: Int
+         let drwtNo6: Int
+         let bnusNo: Int
+         let returnValue: String
+     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
