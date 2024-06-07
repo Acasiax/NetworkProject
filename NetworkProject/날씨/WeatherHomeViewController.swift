f39adceac29cb48501ff35cb6d99edc42255b476
Acasiax <117105267+Acasiax@users.noreply.github.com>
2024-06-07 10:09:40 +0900

    날씨 - 테이블뷰 생성

--- NetworkProject/날씨/WeatherHomeViewController.swift
+++ NetworkProject/날씨/WeatherHomeViewController.swift
@@ -8,26 +8,52 @@
 import UIKit
 
 class WeatherHomeViewController: UIViewController {
-
+    
+    private let dateLabel = UILabel()
+    private let currentLocationLabel = UILabel()
+    private let tableView = UITableView()
+    private var weatherData: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
+        
+        setupUI()
+        loadData()
     }
     
-
+    private func setupUI() {
+    }
+    
+    private func loadData() {
+        weatherData = [
+            "지금은 9℃ 에요",
+            "78% 만큼 습해요",
+            "1m/s의 바람이 불어요",
+            "오늘도 행복한 하루 보내세요"
+        ]
+        tableView.reloadData()
+    }
 }
 
 extension WeatherHomeViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        return weatherData.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        let data = weatherData[indexPath.row]
+        if let url = URL(string: data), UIApplication.shared.canOpenURL(url) {
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: WeatherTableViewCell.identifier, for: indexPath) as? WeatherTableViewCell else {
+                return UITableViewCell()
+            }
+            cell.configure(description: data)
+            return cell
+        } else {
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: WeatherTableViewCell.identifier, for: indexPath) as? WeatherTableViewCell else {
+                return UITableViewCell()
+            }
+            cell.configure(description: data)
+            return cell
+        }
     }
-    
-    
-    
-    
 }
--- NetworkProject/날씨/WeatherTableViewCell.swift
+++ NetworkProject/날씨/WeatherTableViewCell.swift
@@ -7,17 +7,30 @@
 
 import UIKit
 
+
 class WeatherTableViewCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-       
+    static let identifier = "WeatherTableViewCell"
+    
+    private let descriptionLabel = UILabel()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupUI()
     }
     
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:)에서 오류가 났어용")
+    }
+    
+    private func setupUI() {
+        contentView.addSubview(descriptionLabel)
+        descriptionLabel.numberOfLines = 0
+        descriptionLabel.snp.makeConstraints { make in
+            make.edges.equalToSuperview().inset(8)
+        }
+    }
+    
+    func configure(description: String) {
+        descriptionLabel.text = description
+    }
 }
