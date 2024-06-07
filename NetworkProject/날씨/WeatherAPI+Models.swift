6bb00baac5858accde0fc5f7663cfcde148d17e9
Acasiax <117105267+Acasiax@users.noreply.github.com>
2024-06-07 10:57:02 +0900

    날씨 - WeatherAPIModels 클래스 정의,(데이터 관리하는 공간), 타입으로 파일명 생성

---
+++ NetworkProject/날씨/WeatherAPI+Models.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAPI+Models.swift
+//  NetworkProject
+//
+//  Created by 이윤지 on 6/7/24.
+//
+
+import UIKit
+import Alamofire
+import SnapKit
+
+class WeatherAPIModels {
+    
+    static let indentifier = WeatherAPIModels()
+    init() {}
+    private let apiKey = APIKey.weatherKey
+}
--- NetworkProject/날씨/WeatherTableViewCell.swift
+++ NetworkProject/날씨/WeatherTableViewCell.swift
@@ -34,3 +34,4 @@ class WeatherTableViewCell: UITableViewCell {
         descriptionLabel.text = description
     }
 }
+
