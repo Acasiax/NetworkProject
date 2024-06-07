5bc2615185077fac35170ba0920474aa9c2d1d1c
Acasiax <117105267+Acasiax@users.noreply.github.com>
2024-06-07 11:01:40 +0900

    날씨 - URLComponents로 URL 주소,날씨 값 리턴

--- NetworkProject/날씨/WeatherAPI+Models.swift
+++ NetworkProject/날씨/WeatherAPI+Models.swift
@@ -15,11 +15,15 @@ class WeatherAPIModels {
     init() {}
     private let apiKey = APIKey.weatherKey
     
-    private func getURL(){
+    private func getURL(latitude: Double, longitude: Double) -> URL? {
+        var baseURL = URLComponents(string: "https://api.openweathermap.org/data/2.5/weather")
         let latitudeQuery = URLQueryItem(name: "lat", value: String(latitude))
         let longitudeQuery = URLQueryItem(name: "lon", value: String(longitude))
         let lang = URLQueryItem(name: "lang", value: "kr")
         let units = URLQueryItem(name: "units", value: "metric")
         let apiKeyQuery = URLQueryItem(name: "appid", value: apiKey)
+        baseURL?.queryItems = [latitudeQuery, longitudeQuery, lang, units, apiKeyQuery]
+        
+        return baseURL?.url
     }
 }
