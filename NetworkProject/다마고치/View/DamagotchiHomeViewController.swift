0b62fd6aa7bce9b1e487f6e2fc688dfc2970e00c
Acasiax <117105267+Acasiax@users.noreply.github.com>
2024-06-22 16:06:11 +0900

    오토레이아웃

--- NetworkProject/다마고치/View/DamagotchiHomeViewController.swift
+++ NetworkProject/다마고치/View/DamagotchiHomeViewController.swift
@@ -26,6 +26,44 @@ class DamagotchiHomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupCollectionView()
       
     }
+    
+    func setupCollectionView() {
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.backgroundColor = .white
+        collectionViewLayout.scrollDirection = .vertical
+        let cellWidth = (UIScreen.main.bounds.width - 40 - 2 * (3 - 1)) / 3
+        collectionViewLayout.itemSize = CGSize(width: cellWidth, height: cellWidth)
+        collectionViewLayout.minimumInteritemSpacing = 2
+        collectionViewLayout.minimumLineSpacing = 2
+        collectionViewLayout.sectionInset = UIEdgeInsets(top: 30, left: 10, bottom: 10, right: 10)
+        collectionView.collectionViewLayout = collectionViewLayout
+       
+    }
+    func setupViews() {
+        headerLabel.text = "타마고치 선택하기"
+        headerLabel.textAlignment = .center
+        view.addSubview(headerLabel)
+        view.addSubview(collectionView)
+        view.backgroundColor = UIColor(red: 245/255, green: 252/255, blue: 252/255, alpha: 1)
+        collectionView.backgroundColor = UIColor(red: 245/255, green: 252/255, blue: 252/255, alpha: 1)
+    }
+
+}
+
+extension DamagotchiHomeViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+    
+    
+    
+    
 }
